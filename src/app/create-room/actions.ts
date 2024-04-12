a1bfc26546eba9a7263a4059488683dd98ef6c52
fred <fredjuma8@gmail.com>
2024-04-12 18:37:05 +0300

    using jtw and adding id to the  session and room creation when form is submitted

--- src/app/create-room/actions.ts
+++ src/app/create-room/actions.ts
@@ -4,7 +4,7 @@ import { db } from "@/db"
 import { Room, room, } from "@/db/schema"
 import { getSession } from "@/lib/auth";
 
-export async function createRoomAction(roomData: Omit<Room, "userId">) {
+export async function createRoomAction(roomData: Omit<Room, "id" | "userId">) {
     const session = await getSession();
     console.log(session);
 
