85e584240b144e95fee8e5f386e03719b584f216
fred <fredjuma8@gmail.com>
2024-04-13 04:24:39 +0300

    browse page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,67 @@
 import { Button } from "@/components/ui/button";
 import { db } from "@/db";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Room } from "@/db/schema";
+import { GithubIcon } from "lucide-react";
+import { get } from "http";
+import { getRooms } from "@/data-access/rooms";
+
+
+function RoomCard({room}: {room: Room}) {
+  return (
+    <Card>
+  <CardHeader>
+    <CardTitle>{room.name}</CardTitle>
+    <CardDescription>{room.description}</CardDescription>
+  </CardHeader>
+  <CardContent>
+    {room.githubRepo && (
+    <Link 
+    href={room.githubRepo} 
+    className="flex items-center gap-2"
+    target="_blank"
+    rel="noopener noreferrer"
+    >
+      <GithubIcon />
+      Github Project
+      </Link>
+    )}
+  </CardContent>
+  <CardFooter>
+    <Button asChild>
+      <Link href={`/room/${room.id}`}>Join Room</Link>
+    </Button>
+  </CardFooter>
+</Card>
+
+  )
+}
 
 export default async function Home() {
-  const rooms = await db.query.room.findMany();
+  const rooms = await getRooms();
 
   return (
     <main className="min-h-screen p-24">
-
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center mb-8">
       <h1 className="text-4xl">Find Dev Rooms</h1>
       <Button asChild>
         <Link href="/create-room">Create Room</Link>
       </Button>
       </div>
 
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {rooms.map((room) => {
-        return <div key={room.id}>{room.name}</div>
+        return <RoomCard key={room.id} room={room} />;
       })}
+    </div>
     </main>
   );
 }
